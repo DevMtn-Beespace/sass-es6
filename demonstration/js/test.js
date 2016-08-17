f1d64d948db9511e3650d4eef75f91ffb3e58657
mlecke <mlecke@beespace.com>
2016-08-17 14:28:57 -0600

    notes

--- demonstration/js/test.js
+++ demonstration/js/test.js
@@ -1,1 +1,7 @@
 // js test
+var arr = [1,2,3,4,5,6].filter(function(value){
+  return value % 2 === 0;
+});
+
+// ES6
+let arr = [1,2,3,4,5,6].filter(() => value %2 ===0;);
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var babel = require('gulp-babel');
-var plumber = require('gulp-plumber');
+var concat = require('gulp-concat'); // to put everything in one file
+var sass = require('gulp-sass'); // compiles sass
+var babel = require('gulp-babel'); // es6 to es5 compiling
+var plumber = require('gulp-plumber'); // gives error messages for debugging
 
 var paths = {
-  scssSource: './demonstration/styles/**/*.scss',
+  scssSource: './demonstration/styles/**/*.scss', // wildcards
   scssDest: './demonstration/compiled/styles',
-  jsSource: './demonstration/js/**/*.js',
+  jsSource: './demonstration/js/**/*.js', // wildcards
   jsDest: './demonstration/compiled/js'
 };
 
 gulp.task('styles', function() {
   return gulp.src(paths.scssSource)
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sass().on('error', sass.logError)) // .pipe is like next, it's just the next thing to run
   .pipe(concat('styles.css'))
   .pipe(gulp.dest(paths.scssDest));
 });
@@ -32,4 +32,4 @@ gulp.task('watch', function()  {
   gulp.watch(paths.scssSource, ['styles']);
 });
 
-gulp.task('default', ['watch', 'frontjs', 'styles']);
+gulp.task('default', ['watch', 'frontjs', 'styles']); // just type gulp in terminal and it will run all these tasks.
